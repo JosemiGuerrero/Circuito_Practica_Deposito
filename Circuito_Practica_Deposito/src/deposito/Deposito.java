a9ccf2c24d712744226a96c87da2647c1d192c80
JosemiGuerrero <j.guerrero.trejo@iescristobaldemonroy.es>
2023-03-23 09:42:58 +0100

    commit con la documentación en la nueva rama

--- Circuito_Practica_Deposito/src/deposito/Deposito.java
+++ Circuito_Practica_Deposito/src/deposito/Deposito.java
@@ -1,5 +1,12 @@
 package deposito;
 
+/**
+ * Clase deposito que nos ha subio la carmen para hcer pruebas jejejejejeje
+ * 
+ * @author Jozemigué
+ * @version 1.0
+ */
+
 /* Esta clase representa un  depósito cilíndrico donde se almacena aceite  */
 
 public class Deposito {    
@@ -9,12 +16,24 @@ public class Deposito {
     private float altura;
     private String idDeposito;
 
-    //Constructor sin parámetros auxiliar
+   /**
+    * Deposito sin atributos
+    * 
+    * Se crea con atributos a 0 y sin id
+    */
     public Deposito () { //Lo que hace es llamar al constructor con parámetros pasándole valores vacíos
         this(0,0,"");            } //Cierre del constructor
 
 
-    //Constructor de la clase que pide los parámetros necesarios
+    /**
+     * Creamos un deposito con los atributos que le pasamos al constructor
+     * Comprueba que ninguno de los float sea menor o igual a 0 y que el id esté vacío
+     * En ese caso le dará valores predeterminados e imprimirá por consola que ha hecho eso
+     * 
+     * @param valor_diametro valor del diametro introducido
+     * @param valor_altura valor de la altura introducido
+     * @param valor_idDeposito valor del id introducido
+     */
     public Deposito (float valor_diametro, float valor_altura, String valor_idDeposito) {
         if (valor_diametro > 0 && valor_altura > 0) {            
             diametro = valor_diametro;
@@ -27,6 +46,15 @@ public class Deposito {
             System.out.println ("Creado depósito con valores por defecto diametro 10 metros altura 5 metros id 000" );
         }   } //Cierre del constructor
 
+    
+    /**
+     * Método para cambiar los valores del deposito.
+     * Si los valores son menores o igual a 0 o el id es vacio se inicializará todo a 0 e id vacio
+     * 
+     * @param valor_idDeposito valor del id introducido
+     * @param valor_diametro valor del diametro introducido
+     * @param valor_altura valor de la altura introducido
+     */
     public void setValoresDeposito (String valor_idDeposito, float valor_diametro, float valor_altura) {
         idDeposito = valor_idDeposito;
         diametro = valor_diametro;
@@ -40,7 +68,19 @@ public class Deposito {
             altura = 0;
         }     } //Cierre del método
 
+    
+   /**
+    * Devuelve el diametro del depósito
+    *  
+    * @return diametro del deposito
+    */
     public float getDiametro () { return diametro; } //Método de acceso
+    
+    /**
+     * Devuelve la altura del deposito
+     * 
+     * @return altura del deposito
+     */
     public float getAltura () { return altura; } //Método de acceso
     public String getIdDeposito () { return idDeposito; } //Método de acceso
     public float valorCapacidad () { //Método tipo función
